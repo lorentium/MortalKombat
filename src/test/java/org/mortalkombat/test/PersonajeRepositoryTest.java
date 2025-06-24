48684066798bc95226485f4e7f57e7254a331b02
Lorenzo Lopez <l.lopez14@ufromail.cl>
2025-06-23 20:30:25 -0400

    test: metodos de busqueda con filtros

--- src/test/java/org/mortalkombat/test/PersonajeRepositoryTest.java
+++ src/test/java/org/mortalkombat/test/PersonajeRepositoryTest.java
@@ -14,6 +14,7 @@ import static org.mortalkombat.NivelPoder.*;
 public class PersonajeRepositoryTest {
     private Personaje personaje1;
     private Personaje personaje2;
+    private Personaje personaje3;
     private PersonajeRepository repo;
 
 
@@ -22,15 +23,45 @@ public class PersonajeRepositoryTest {
         repo = new PersonajeRepository();
         personaje1 = new Personaje("Scorpion", 100, ALTO, new ArrayList<>());
         personaje2 = new Personaje("Sub-Zero", 120, MEDIO, new ArrayList<>());
+        personaje3 = new Personaje("Kitara", 50, BAJO, new ArrayList<>());
         repo.agregarPersonaje(personaje1);
         repo.agregarPersonaje(personaje2);
 
     }
-    
+
     @Test
     public void testAgregarPersonaje() {
         repo.agregarPersonaje(personaje1);
         assertTrue(repo.obtenerPersonajes().contains(personaje1));
+    }
+
+    @Test
+    public void testObtenerPorNivelPoderMinimo() {
+        // personaje1: ALTO, personaje2: MEDIO, personaje3: BAJO
+        var resultado = repo.obtenerPorNivelPoderMinimo(MEDIO);
+        assertTrue(resultado.contains(personaje1));
+        assertTrue(resultado.contains(personaje2));
+        assertFalse(resultado.contains(personaje3));
+        assertEquals(2, resultado.size());
+
+        resultado = repo.obtenerPorNivelPoderMinimo(ALTO);
+        assertTrue(resultado.contains(personaje1));
+        assertFalse(resultado.contains(personaje2));
+        assertFalse(resultado.contains(personaje3));
+
+
+        assertEquals(1, resultado.size());
+    }
+
+    @Test
+    public void testBuscarPorNombre() {
+        var resultado = repo.buscarPorNombre("scor");
+        assertTrue(resultado.contains(personaje1));
+        assertFalse(resultado.contains(personaje2));
+
+        resultado = repo.buscarPorNombre("SUB");
+        assertTrue(resultado.contains(personaje2));
+        assertFalse(resultado.contains(personaje1));
     }
 
     @Test
